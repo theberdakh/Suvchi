1c03bcc2b9cb7060e6b9799c24057d76355ce7d5
theberdakh <theberdakh@gmail.com>
2024-03-01 17:10:24 +0500

    add simple login validator

--- app/src/main/java/com/theberdakh/suvchi/ui/LoginFragment.kt
+++ app/src/main/java/com/theberdakh/suvchi/ui/LoginFragment.kt
@@ -8,6 +8,8 @@ import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 import com.theberdakh.suvchi.R
 import com.theberdakh.suvchi.data.local.pref.LocalPreferences
 import com.theberdakh.suvchi.databinding.FragmentLoginBinding
@@ -19,7 +21,7 @@ import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.launch
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class LoginFragment: Fragment() {
+class LoginFragment : Fragment() {
     private var _binding: FragmentLoginBinding? = null
     private val binding get() = checkNotNull(_binding)
     private val viewModel by viewModel<LoginViewModel>()
@@ -31,29 +33,54 @@ class LoginFragment: Fragment() {
     ): View {
         _binding = FragmentLoginBinding.inflate(inflater, container, false)
 
+        toggleVisibilityLoginButton()
+        initObservers()
+
+        binding.loginButton.setOnClickListener {
+            if (checkEditText(binding.passwordLayout, binding.passwordEditText) && checkEditText(binding.userNameLayout, binding.usernameEditText)) {
+                lifecycleScope.launch {
+                    viewModel.login(
+                        binding.usernameEditText.text.toString(),
+                        binding.passwordEditText.text.toString()
+                    )
+                }
+            }
+        }
+
+        return binding.root
+    }
+
+    private fun toggleVisibilityLoginButton() {
         binding.passwordEditText.doAfterTextChanged {
-            if (!it.isNullOrEmpty() && it.isNotBlank()){
+            if (!it.isNullOrEmpty() && it.isNotBlank()) {
                 binding.loginButton.show()
             }
         }
+    }
 
-        initObservers()
-        binding.loginButton.setOnClickListener {
-            lifecycleScope.launch {
-                viewModel.login(binding.usernameEditText.text.toString(), binding.passwordEditText.text.toString())
-            }
-           // add password, login validator?
+    private fun checkEditText(
+        textInputLayout: TextInputLayout,
+        editText: TextInputEditText
+    ): Boolean {
+
+        editText.doAfterTextChanged {
+            textInputLayout.error = null
         }
 
-
-
-        return binding.root
+        return if (editText.text.toString().isBlank() || editText.text.toString().isEmpty()) {
+            textInputLayout.error = null
+            textInputLayout.error = "Maydon bósh bólishi mumkin emas"
+            false
+        } else {
+            true
+        }
     }
 
     private fun initObservers() {
         viewModel.responseIsSuccessful.onEach {
             LocalPreferences().isLoggedIn = true
-            val navHostFragment = requireActivity().supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+            val navHostFragment =
+                requireActivity().supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
             val inflater = navHostFragment.navController.navInflater
             val graph = inflater.inflate(R.navigation.parent_nav)
             navHostFragment.navController.graph = graph
